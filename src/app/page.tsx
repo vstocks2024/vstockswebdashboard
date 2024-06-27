e8f671e8c1c447cd9ea598ce2d7adf3aa1fcfba8
vstocks2024 <vstocks2024@gmail.com>
2024-06-27 19:50:53 +0530

    dashboard initialized

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
-import Image from "next/image";
+import ECommerce from "@/components/Dashboard/E-commerce";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLaout";
+import React from "react";
+import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title:"",
+  description: "",
+};
 
 export default function Home() {
-  return (<></>);
+  redirect("/customers");
+  return (
+    <>
+      <DefaultLayout>
+        <ECommerce />
+      </DefaultLayout>
+    </>
+  );
 }
